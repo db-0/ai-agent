20e20e4c64b3f70ccd25270fbd69c16172de59f8
db-0 <dan@dbsphere.net>
2025-07-13 19:51:44 -0600

    added api and basic token handling

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
-def main():
-    print("Hello from ai-agent!")
+client = genai.Client(api_key=api_key)
+content = "Why is Boot.dev such a great place to learn backend development? Use on paragraph maximum."
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=content)
 
+def main():
+    print(response.text + "\n")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
