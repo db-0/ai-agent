d9f60ac7aa7d73f5c1763fa81b7ab9f1291e1d29
db-0 <dan@dbsphere.net>
2025-07-15 07:57:22 -0600

    argument handling and message list framework

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
-content = "Why is Boot.dev such a great place to learn backend development? Use on paragraph maximum."
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=content)
 
 def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI-Agent")
+        print('\nUsage: python main.py "your prompt here"')
+        sys.exit(1)
+    
+    if args[-1] == "--verbose":
+        verbose = True
+        args = args[:-1]
+    else:
+        verbose = False
+    
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages)
+    
     print(response.text + "\n")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
